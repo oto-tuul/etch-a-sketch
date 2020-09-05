3262763ab23497981b0b0db95dd206a6ebdf8666
oto-tuul <oto.tuul@gmail.com>
2020-09-05 22:57:59 +0300

    Added divs and set these in a 16x16 grid

---
+++ README.md
@@ -0,0 +1,3 @@
+#etch-a-sketch
+
+An Etch-a-sketch application using HTML, CSS and JavaScript.
---
+++ etch.js
@@ -0,0 +1,6 @@
+for (i = 0; i < 256; i++) {
+    const container = document.querySelector('#container');
+    const square = document.createElement('div');
+    square.id = 'square'
+    container.appendChild(square);
+}
