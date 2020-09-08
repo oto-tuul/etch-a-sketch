249857b6c4047723e48cc3daefe5db585fd3223d
oto-tuul <oto.tuul@gmail.com>
2020-09-09 00:30:11 +0300

    Added colouring to an eventListener on mouseenter Added a button to clear the grid and promt for a new grid size Added a function to create a new grid with defined size

--- etch.js
+++ etch.js
@@ -1,6 +1,32 @@
-for (i = 0; i < 256; i++) {
-    const container = document.querySelector('#container');
-    const square = document.createElement('div');
-    square.id = 'square'
-    container.appendChild(square);
+let squareNum = 256
+let side = 16
+function sketch(squareNum) {
+    for (i = 0; i < squareNum; i++) {
+        const container = document.querySelector('#container');
+        const square = document.createElement('div');
+        square.classList.add('square')
+        container.appendChild(square);
+    }
 }
+sketch(squareNum);
+
+document.querySelectorAll('.square').forEach(square => {
+    square.addEventListener('mouseenter', function(){
+        event.target.style.backgroundColor = 'black';
+    
+    });
+});
+
+document.getElementById('clrBtn').addEventListener('click', function(){
+    document.querySelectorAll('.square').forEach(square => {
+        square.style.backgroundColor = 'white';
+    });
+    side = prompt('How many squares per side?');
+    squareNum = side ** 2;
+    document.querySelectorAll('.square').forEach(square => {
+        container.removeChild(square);
+    });
+    sketch(squareNum);
+    document.querySelector('#container').style.gridTemplateRows = `repeat(${side}, 1fr)`;
+    document.querySelector('#container').style.gridTemplateColumns = `repeat(${side}, 1fr)`;
+});
