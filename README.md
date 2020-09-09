3494a3de2c084030dbaa86518faff61d6dd4390d
oto-tuul <oto.tuul@gmail.com>
2020-09-10 00:15:23 +0300

    Added a 'colourful mode' for colouring the squares with random RGB after a button press. Added an 'additive mode' for coloring the squares black additively by 10% on each mouseover.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 #etch-a-sketch
 
-An Etch-a-sketch application using HTML, CSS and JavaScript.
+An Etch-a-sketch application using HTML, CSS and JavaScript for The Odin Project.
+
+Draws and a 16x16 grid on load, squares are colured balck on mouseenter event, with additional modes of random RGB colors and additive coloring.
--- etch.js
+++ etch.js
@@ -7,15 +7,16 @@ function sketch(squareNum) {
         square.classList.add('square')
         container.appendChild(square);
     }
+    document.querySelectorAll('.square').forEach(square => {
+        square.addEventListener('mouseenter', function(){
+            event.target.style.backgroundColor = 'black';
+        
+        });
+    });
 }
 sketch(squareNum);
 
-document.querySelectorAll('.square').forEach(square => {
-    square.addEventListener('mouseenter', function(){
-        event.target.style.backgroundColor = 'black';
-    
-    });
-});
+
 
 document.getElementById('clrBtn').addEventListener('click', function(){
     document.querySelectorAll('.square').forEach(square => {
@@ -29,4 +30,25 @@ document.getElementById('clrBtn').addEventListener('click', function(){
     sketch(squareNum);
     document.querySelector('#container').style.gridTemplateRows = `repeat(${side}, 1fr)`;
     document.querySelector('#container').style.gridTemplateColumns = `repeat(${side}, 1fr)`;
+});
+
+document.getElementById('colrBtn').addEventListener('click', function(){
+    document.querySelectorAll('.square').forEach(square => {
+        square.style.backgroundColor = 'white';
+        square.addEventListener('mouseenter', function(){
+            event.target.style.backgroundColor = 'hsl(' + 360 * Math.random() + ', 100%, 50%)';
+        });
+    });
+});
+
+document.getElementById('addBtn').addEventListener('click', function(){
+    document.querySelectorAll('.square').forEach(square => {
+        square.style.backgroundColor = 'white';
+        let perCol = 100
+        square.addEventListener('mouseenter', function(){
+            
+            event.target.style.backgroundColor = `hsl(0, 0%, ${perCol -= 10}%)`;
+            console.log('mouse');
+        });
+    });
 });
